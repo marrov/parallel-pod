1ad7a0244ea5e493f93582c6d56d3f4a6bb4d521
Kristian Ronnberg <kriron@kth.se>
2019-09-13 15:50:02 +0200

    Use variable to store cpp binary file name everywhere

--- testCase/runTestCaseCpp.sh
+++ testCase/runTestCaseCpp.sh
@@ -3,14 +3,14 @@
 printf "Making directories if not present...\n\n"
 mkdir -p chronos/ mode/ VTK/
 
-cppbin="./3DPOD.out"
-if [ -e "$cppbin" ]
+cppbin="3DPOD.out"
+if [ -e ./"$cppbin" ]
 then
     printf "Running 3D POD script...\n\n"
-    time $cppbin
+    time ./$cppbin
     printf "Done \n"
 else
     printf "Compiling and running 3D POD script...\n\n"
-    g++ -std=c++11 -O3 -I ./eigen 3DPOD.cpp -o 3DPOD.out; time ./3DPOD.out
+    g++ -std=c++11 -O3 -I ./eigen 3DPOD.cpp -o $cppbin; time ./$cppbin
     printf "Done \n"
 fi
