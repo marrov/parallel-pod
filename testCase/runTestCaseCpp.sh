07a73ae64f3dae64256e5db85c955a54a8ec179c
mrovirasacie <marcrovirasacie@gmail.com>
2019-10-01 17:10:25 +0200

    Implemented Spectral POD option via in-code flags

--- testCase/runTestCaseCpp.sh
+++ testCase/runTestCaseCpp.sh
@@ -22,15 +22,16 @@ mkdir -p output/chronos/ output/mode/ output/VTK/
 if [ -e ./"$cppbin" ]
 then
     printf "Running 3D POD script...\n\n"
-    time ./$cppbin -i ./input -c output/chronos -m output/mode -p 381600 -v 1 -nm 5 -s 100 -np 6
+    time ./$cppbin -i ./input -c output/chronos -m output/mode -p 381600 -v 3 -nm 5 -s 200 -np 6
 
     # Matlab sort modes
-    printf "\nRunning Matlab sorting script for modes...\n"
-    matlab -nodisplay -nosplash -nodesktop -r "run('sortModes.m');exit;"
+    #printf "\nRunning Matlab sorting script for modes...\n"
+    #matlab -nodisplay -nosplash -nodesktop -r "run('sortModes.m');exit;"
 
     # Python sorted modes to VTK
-    printf "\nRunning Python mode to VTK script...\n\n"
-    python2.7 modeToVTK_U.py
+    #printf "\nRunning Python mode to VTK script...\n\n"
+    #python2.7 modeToVTK_U.py
+    
     exit 0
 else
     printf "$cppbin not found. Please compile and install.\n"
